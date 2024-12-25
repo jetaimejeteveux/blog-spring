bab2e7a86d3b313eca22965e5eb8b7d67fddc7d5
jetaimejeteveux <akufirman22@gmail.com>
2024-12-25 07:17:53 +0700

    init project

---
+++ src/main/java/org/belajar/blogmaven/BlogMavenApplication.java
@@ -0,0 +1,13 @@
+package org.belajar.blogmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/belajar/blogmaven/BlogMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.belajar.blogmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
